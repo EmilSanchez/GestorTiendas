f646810c1f69677e888630d60c2313dd51568a79
EmilSanchez <131091589+EmilSanchez@users.noreply.github.com>
2026-05-14 14:23:23 -0500

    Add edit button to task actions

--- js/mod-tareas.js
+++ js/mod-tareas.js
@@ -166,6 +166,9 @@ function _renderNoCumplidas(noCumplidas) {
               <button class="btn btn-ghost btn-icon btn-sm" title="Marcar como cumplida" onclick="_marcarNoCumplidaComoHecha('${t.id}')">
                 <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="var(--green)" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
               </button>
+              <button class="btn btn-ghost btn-icon btn-sm" onclick="openModalTarea('${t.id}')" title="Editar">
+                <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
+              </button>
               <button class="btn btn-ghost btn-icon btn-sm" onclick="_confirmarDeleteTarea('${t.id}')" title="Eliminar" style="color:var(--red);" onmouseover="this.style.background='var(--red-bg)'" onmouseout="this.style.background=''">
                 <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"/><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/></svg>
               </button>
